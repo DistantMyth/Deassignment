13f1b5b5d6c1cc3bbf571ecc5506e353de94b5bd
Tarun Kumar <thetarun3@gmail.com>
2026-06-02 16:45:15 +0530

    deleted usless stuff

--- temp/PrintEvenNumbers.java
+++
@@ -1,8 +0,0 @@
-public class PrintEvenNumbers {
-    public static void main(String[] args) {
-        for (int i = 2; i <= 100; i += 2) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-}
